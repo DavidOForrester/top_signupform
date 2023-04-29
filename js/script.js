b0d4a8f898dd98f79b3515f906227f8f580dc0e9
David Forrester <david.o.forrester@gmail.com>
2023-04-29 14:18:41 +1000

    Adding js to check the passwords match

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-// Variables
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  const password1 = document.getElementById("password").value;
+  const password2 = document.getElementById("confirm_password").value;
 
-
-// Code body
-
-
-// Functions
-
+  if (password1 === password2) {
+    // Allow form submission
+  } else {
+    // Prevent form submission and display error message
+    event.preventDefault();
+    alert("Passwords do not match");
+  }
+});
